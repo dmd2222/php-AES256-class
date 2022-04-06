01a58df373ca76c86b1f7fed4305722a8a6a4354
dmd2222 <dmd20@gmx.de>
2022-04-06 19:30:36 +0200

    Add files via upload

---
+++ AES256_class.php
@@ -0,0 +1,132 @@
+<?php
+
+
+
+
+
+//Testscript
+/*
+$loops=10;
+$encryption_key=AES256::generate_random_string(4,16);
+$text_string="Test text 123" ;
+echo $text_string . "<br>";
+
+$a =AES256::encrypt($text_string,$loops,$encryption_key);
+
+echo "<br>Transmitting: Length: " . strlen($text_string) . " =>" . strlen($a[0]) . "<br>";
+var_dump($a) ;
+echo "<br>";
+echo "<br>";
+
+$b = AES256::decrypt($a[0],$loops,$encryption_key,$a[1]);
+
+echo $b . "<br>";
+
+
+*/
+
+
+class AES256{
+
+
+    //public functions
+   public static  function encrypt($text_string,$number_loops_int=1,$encryption_key ='0123456789'){
+    
+        $encryption_iv=NULL;
+        $encryption =$text_string;
+
+        for($i = 0; $i < $number_loops_int; ++$i) {
+            
+
+        $encryption_iv[$i] = self::random_bytes(16);
+
+        // Encryption of string process starts
+        $encryption = openssl_encrypt($encryption, 'AES-256-CBC',
+        $encryption_key,0, $encryption_iv[$i]);
+
+
+
+        }
+
+        return array($encryption,$encryption_iv);
+
+}
+
+
+
+
+public static function decrypt($encrypted_text_string,$number_loops_int=1,$encryption_key = '0123456789',$encryption_iv){
+
+
+$decryption = $encrypted_text_string;
+
+for($i = 0; $i < $number_loops_int; ++$i) {
+
+// Encryption of string process starts
+$decryption = openssl_decrypt($decryption, 'AES-256-CBC',
+$encryption_key,0, $encryption_iv[$number_loops_int-$i-1]);
+
+}
+
+return  $decryption;
+}
+
+
+public static function generate_random_string( $option, $length ){
+    $permitted_chars="";
+    switch ($option) {
+        case 0:
+            $permitted_chars = '0123456789';
+            break;
+        case 1:
+            $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyz';
+            break;
+        case 2:
+            $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            break;
+        case 3:
+            $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"§$%&/()=?*+#<>';
+            break;
+        case 4:
+            $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"§$%&/()=?*+#<>äüöÄÜÖ';
+            break;
+        default:
+            $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyz';
+    }
+    //generated string
+    return  substr(str_shuffle($permitted_chars), 0, $length);
+}
+
+
+
+
+
+
+ //private function
+    private function __construct()
+    {
+        //prevents creating object of class.
+    }
+
+
+    
+private static function random_bytes($length = 6)
+{
+    $characters = '0123456789';
+    $characters_length = strlen($characters);
+    $output = '';
+    for ($i = 0; $i < $length; $i++)
+        $output .= $characters[rand(0, $characters_length - 1)];
+
+    return $output;
+}
+
+
+
+
+}
+
+
+
+
+?>
